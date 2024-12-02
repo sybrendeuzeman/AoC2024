18af5b1b4c5386ed3df955880ea19a9b07ae5d0e
Sybren Deuzeman <s.deuzeman@tweedekamer.nl>
2024-12-02 16:10:45 +0100

    Oplossing voor dag 2

---
+++ 2/puzzle_2.py
@@ -0,0 +1,86 @@
+
+with open('input.txt', 'r') as f:
+    list_reports = f.readlines()
+
+list_report_numbers = [
+    [
+        int(number)
+        for number
+        in report.replace('\n', '').split(' ')
+    ] 
+    for report
+    in list_reports
+]
+
+
+# Problem 1:
+# Define function to check a single report
+def check_report(report_numbers):
+    # Set sign decr or incr
+    if report_numbers[0] > report_numbers[1]:
+        sign = -1
+    elif report_numbers[0] < report_numbers[1]:
+        sign = 1
+    elif report_numbers[0] == report_numbers[1]:
+        return False, 0 # No increase between first two integers
+    else:
+        raise Exception(f"Setting of sign had unexpected result for {report}")
+    
+    # Check sequence
+    number_prev = report_numbers[0]
+    for i in range(1,len(report_numbers)):
+        number_curr = report_numbers[i]
+        if sign*(number_curr - number_prev) in [1,2,3]:
+            number_prev = number_curr
+            continue # Check succeeded
+        else:
+            return False, i # Check failed, no need to check more
+        
+
+    return True, i # All checks succeeded.
+
+# Check all reports
+list_check_report = [
+    check_report(report_numbers)[0]
+    for report_numbers
+    in list_report_numbers
+]
+
+# Get number of succeeded checks
+number_succeeded_checks = sum(list_check_report)
+
+print(f'Number of succeeded checks: {number_succeeded_checks}')
+
+#Problem 2:
+
+# Wrapper to implement the dampener
+def check_with_dampener(report_numbers):
+    first_check = check_report(report_numbers)
+
+    if first_check[0] == True:
+        return True
+    
+    for i_del in range(0,len(report_numbers)):
+        
+        report_temp = report_numbers[:]
+
+        del report_temp[i_del]
+        #print(f'Report after del: {report_temp}')
+        
+        check_after_delete = check_report(report_temp)
+
+        if check_after_delete[0] == True:
+            return True
+        
+    return False
+
+# Get list with checks
+list_check_dampened = [
+    check_with_dampener(report_numbers)
+    for report_numbers
+    in list_report_numbers
+]
+
+number_succeeded_checks_dampened = sum(list_check_dampened)
+
+print(f'Number of succeeded checks with dampener: {number_succeeded_checks_dampened}')
