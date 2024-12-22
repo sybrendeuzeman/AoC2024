17ac740c961831319983aa8a7788d8b48e85b4a9
Sybren Deuzeman <s.deuzeman@tweedekamer.nl>
2024-12-22 18:16:12 +0100

    Added comments in part 2 (part 1 is completely obvious...)

--- 22/puzzle_22.py
+++ 22/puzzle_22.py
@@ -8,6 +8,7 @@ def next_secret(secret):
 
     return(secret)
 
+# Part 1
 sum_sec = 0
 for inp in input:
     secret = inp
@@ -19,30 +20,49 @@ for inp in input:
 print(sum_sec)
 
 # Part 2
+
+# Fill a dictionary for every preceding sequence of changes
 banana_with_changes = {}
 
-
+# Add possible banana's with all monkeys
 for inp in input:
 
     secret = inp
+
+    # Initialize the old prize
     old_prize = secret % 10
+    
+    # Keep track of last 4 changes
     last_changes = []
+    
+    # Keep track of changes for which banana's where already bought
     already_bought = set()
     
     for i in range(2000):
+        # Get the next secret number
         secret = next_secret(secret)
+
+        # Get prize 
         new_prize = secret % 10
 
+        # Add to list with last changes
         last_changes.append(new_prize - old_prize)
         
+        # Add number of banana's to dictionary if there are 4 last changes
         if (len(last_changes) == 4):
+            # Make the list hashable for the dictionary
             key = ','.join([str(i) for i in last_changes])
-            if (key not in already_bought):
-                banana_with_changes[key] = banana_with_changes.get(key, 0) + new_prize
-                already_bought.add(key)
-            last_changes = last_changes[1:]
-        elif len(last_changes) >= 4:
-            print('Last changes not')
 
+            # Only possible to buy if not yet bought with sequence at this monkey
+            if (key not in already_bought):
+                # Add the  prize to the dictionary at the key
+                banana_with_changes[key] = banana_with_changes.get(key, 0) + new_prize
+                # Add the key to the set of sequences where you already bought
+                already_bought.add(key)
+
+            # Remove the first of the last changes from the list
+            last_changes = last_changes[1:]
+        
+        # Set old prize is new prize
         old_prize = new_prize
 print('Maximum amount of bananas is:', max(banana_with_changes.values()))
